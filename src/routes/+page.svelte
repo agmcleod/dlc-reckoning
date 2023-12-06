be69d493a57f9b1b0a064a83c912b8dd8b855130
aaron mcleod <aaron.g.mcleod@gmail.com>
2023-12-05 22:37:23 -0500

    copy update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,10 +30,13 @@
     The <a href="https://www.patreon.com/dlcpod">DLC</a> video game podcast is a weekly show hosted
     by Jeff Cannata and Christian Spicer. They cover gaming news, new releases, and have a guest on
     each week to share other perspectives. At the beginning of each year, the two hosts go over
-    their major predictions in gaming for the year. They pick a number of <em>Bold</em> predictions.
-    And then they really go bananas with their <em>Cool Ranch</em> predictions. In these episodes
-    they also review their past predictions to determine if they were <strong>core-rect</strong> or
-    if they got <strong>rekt</strong>.
+    their major predictions in gaming for the coming year. They pick a number of <em>Bold</em>
+    predictions. Which are their maybe unlikely predictions, but still possible. And then they really
+    go bananas with their <em>Cool Ranch</em> predictions. Which are completely off the wall
+    predictions of what will happen in the coming year. Some might even say they've gone too far. In
+    these episodes they also review their past predictions to determine if they were
+    <strong>core-rect</strong>
+    or if they got <strong>rekt</strong>.
   </p>
   <p>
     This is a fan made, non-affiliated site displaying their current predictions, past predictions,
